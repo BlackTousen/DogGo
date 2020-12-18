89da293161e5995be644a4276dd92d1e247b5cc6
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-12-18 15:11:31 -0600

    minor changes

---
+++ DogGo/Models/ViewModels/WalkerProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models.ViewModels
+{
+    public class WalkerProfileViewModel
+    {
+        public Walker Walker { get; set; }
+        public List<Walk> Walks { get; set; }
+        public string TotalTimeWalkedDisplay { get; set; }
+        public string WalkTimeDisplay
+        {
+            get
+            {
+                int totalWalkSeconds = Walks.Sum(w => w.Duration);
+                TimeSpan walkTime = TimeSpan.FromSeconds(totalWalkSeconds);
+                string walkTimeDisplay = $"{walkTime.Hours}hr {walkTime.Minutes}min";
+                return walkTimeDisplay;
+            }
+        }
+    }
+}
